dcd0a750ae84f554dbb7d86ff72106bb582d9e6d
nice-shot <shefferu@gmail.com>
2014-12-27 19:42:14 +0200

    added the basic building blocks for bootstrap (so much easier than foundation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    wiredep: {
+      task: {
+        src: ['index.html'],
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-wiredep');
+
   grunt.registerTask('default', []);
 }
