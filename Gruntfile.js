3b5457345678a9855a336ee86983c1db99cb0ae7
nice-shot <shefferu@gmail.com>
2014-12-27 21:06:41 +0200

    added a multilanguage structure so that hebrew and english versions will come out from the same files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,36 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     wiredep: {
       task: {
-        src: ['index.html'],
+        src: ['build/*.html'],
+      }
+    },
+    multi_language: {
+      translate: {
+        resources: 'src/lang/',
+        options: {
+          tag: '{{ }}',
+          src: 'src/html/index.html',
+          dest: 'build'
+        }
+      }
+    },
+    replace: {
+      task: {
+        src: ['build/*.en.html'],
+        overwrite: true,
+        replacements: [
+          {
+            from: /.*bootstrap-rtl\.css.*/,
+            to: ''
+          }
+        ]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-multi-language');
+  grunt.loadNpmTasks('grunt-text-replace');
 
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['multi_language', 'wiredep', 'replace']);
 }
