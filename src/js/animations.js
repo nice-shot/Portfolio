b57236f72f407efb670beb80f9574c738a8277ca
nice-shot <shefferu@gmail.com>
2015-01-02 14:02:12 +0200

    Finally managed to create a write-on effect for a single letter. Need to find a smart way to automate it

--- src/js/animations.js
+++ src/js/animations.js
@@ -2,6 +2,8 @@ var Effects = (function (eff) {
   /*
   Adds the typeIn effect for the programming title
   */
+  "use strict";
+
   eff.programmingTypeIn = function () {
     var programmingTitle = $("#interests > #programming > h3 > span");
     var value = programmingTitle.text();
@@ -11,6 +13,29 @@ var Effects = (function (eff) {
       typeSpeed: 35,
       startDelay: 1000,
     });
+  };
+
+  return eff;
+} (Effects || {}));
+
+
+var Effects = (function (eff) {
+  /*
+  Adds the write-on effect for the design title
+  */
+  "use strict";
+
+  eff.designWriteOn = function () {
+    var designTitle = $("#interests > #design > h3");
+    var value = designTitle.text();
+    designTitle.text("");
+    for (var i = 0; i < value.length; i++) {
+      var span = $("<span>");
+      span.attr("id", "design-letter-" + (i+1));
+      span.text(value[i]);
+      designTitle.append(span);
+      span.addClass("write");
+    }
   };
 
   return eff;
