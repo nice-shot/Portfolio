bad882df2039774f50e9a0b2b58c46e7003c9cf5
nice-shot <shefferu@gmail.com>
2015-02-28 12:05:26 +0200

    added the games pages and added the ultimate election and the generation catch games

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,114 +1,115 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    wiredep: {
-      task: {
-        src: ['**/*.html'],
-        cwd: 'build',
-      }
-    },
-    watch: {
-      files: [
-        'Gruntfile.js',
-        'src/**',
-      ],
-      tasks: ['default'],
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/js/*.js'],
-    },
-    concat: {
-      buildJs: {
-        src: ['src/js/**/*.js'],
-        dest: 'build/js/main.js'
-      },
-    },
-    replace: {
-      task: {
-        src: ['build/en/*.html'],
-        overwrite: true,
-        replacements: [
-          // Remove bootstrap-rtl from english html files
-          {
-            from: /.*bootstrap-rtl\.css.*/,
-            to: ''
-          },
-        ]
-      },
-    },
-    copy: {
-      main: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/assets',
-            src: ['**'],
-            dest: 'build/assets',
-            //flatten: true,
-          },
-          {
-            expand: true,
-            src: ['bower_components/**'],
-            dest: 'build',
-          },
-          {
-            expand: true,
-            src: ['bower.json'],
-            dest: 'build'
-          }
-        ]
-      }
-    },
-    sass: {
-      buildCss: {
-        files: {
-          'build/css/main.css': 'src/css/main.scss',
-        }
-      }
-    },
-    jade: {
-      compile: {
-        options: {
-          pretty: true,
-          i18n: {
-            locales: 'src/locales/*.json',
-            namespace: '$in'
-          }
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        wiredep: {
+            task: {
+                src: ['**/*.html'],
+                cwd: 'build',
+            }
         },
-        files: {
-          'build/index.html': 'src/jade/index.jade',
-          'build/videos.html': 'src/jade/videos.jade',
-          'build/sketches.html': 'src/jade/sketches.jade',
-          'build/drawings.html': 'src/jade/drawings.jade',
-        }
-      },
-      referrer: {
-        options: {
-          pretty: true
+        watch: {
+            files: [
+                'Gruntfile.js',
+                'src/**',
+            ],
+            tasks: ['default'],
         },
-        files: {
-          'build/index.html': 'src/jade/referrer.jade',
+        jshint: {
+            files: ['Gruntfile.js', 'src/js/*.js'],
+        },
+        concat: {
+            buildJs: {
+                src: ['src/js/**/*.js'],
+                dest: 'build/js/main.js'
+            },
+        },
+        replace: {
+            task: {
+                src: ['build/en/*.html'],
+                overwrite: true,
+                replacements: [
+                    // Remove bootstrap-rtl from english html files
+                    {
+                        from: /.*bootstrap-rtl\.css.*/,
+                        to: ''
+                    },
+                ]
+            },
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/assets',
+                        src: ['**'],
+                        dest: 'build/assets',
+                        //flatten: true,
+                    },
+                    {
+                        expand: true,
+                        src: ['bower_components/**'],
+                        dest: 'build',
+                    },
+                    {
+                        expand: true,
+                        src: ['bower.json'],
+                        dest: 'build'
+                    }
+                ]
+            }
+        },
+        sass: {
+            buildCss: {
+                files: {
+                    'build/css/main.css': 'src/css/main.scss',
+                }
+            }
+        },
+        jade: {
+            compile: {
+                options: {
+                    pretty: true,
+                    i18n: {
+                        locales: 'src/locales/*.json',
+                        namespace: '$in'
+                    }
+                },
+                files: {
+                    'build/index.html': 'src/jade/index.jade',
+                    'build/videos.html': 'src/jade/videos.jade',
+                    'build/sketches.html': 'src/jade/sketches.jade',
+                    'build/drawings.html': 'src/jade/drawings.jade',
+                    'build/games.html': 'src/jade/games.jade',
+                }
+            },
+            referrer: {
+                options: {
+                    pretty: true
+                },
+                files: {
+                    'build/index.html': 'src/jade/referrer.jade',
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-wiredep');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-text-replace');
-  grunt.loadNpmTasks('grunt-jade-i18n');
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-jade-i18n');
 
-  grunt.registerTask('default', [
-    'jshint',
-    'concat',
-    'sass',
-    'copy',
-    'jade',
-    'wiredep',
-    'replace',
-  ]);
+    grunt.registerTask('default', [
+        'jshint',
+        'concat',
+        'sass',
+        'copy',
+        'jade',
+        'wiredep',
+        'replace',
+    ]);
 };
